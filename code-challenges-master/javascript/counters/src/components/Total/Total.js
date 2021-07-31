1e9bb2edd5fce99d9e792d2a1a0fff8d9c1b639e
Irene Perez Alonso <irperal@hotmail.com>
2021-07-31 18:01:09 +0200

    Modify the styles in SCSS

--- code-challenges-master/javascript/counters/src/components/Total/Total.js
+++ code-challenges-master/javascript/counters/src/components/Total/Total.js
@@ -1,9 +1,12 @@
 import React from 'react';
-// import './Total.css';
+import './Total.css';
 
 function Total({ totalValue }) {
   return (
-    <div>{totalValue}</div>
+    <div className="app-totalValue">
+      <h1 className="app-totalValue__tittle">Total Value:</h1>
+      <div className="app-totalValue__number">{totalValue}</div>
+    </div>
   )
 }
 
